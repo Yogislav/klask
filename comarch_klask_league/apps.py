8761d141d64fd8b413e06a8c333c9f25816126f5
Michał <mmarczy@interia.eu>
2018-12-05 14:46:18 +0000

    Ready to deployment

--- comarch_klask_league/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ComarchKlaskLeagueConfig(AppConfig):
-    name = 'comarch_klask_league'
--- klask/wsgi.py
+++ klask/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "zdm_ebok.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "klask.settings")
 
 application = get_wsgi_application()
